28ea17cd644ea7780fcb1f678eed1a349fa07311
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-05 16:35:06 -0500

    Stage 1

---
+++ main.js
@@ -0,0 +1,142 @@
+a {
+	text-decoration: none;
+	color: white;
+}
+
+body {
+	background: rgb(39,48,63);
+}
+
+.navbar {
+	height: 50px;
+	background: #1477B3;	
+}
+
+/*.titlelogo {
+	height: 35px;
+	background: white;
+}
+
+.titlelink {
+	height: 35px;
+	background: white;
+}*/
+
+.login {
+	float: right;
+	height: 50px;
+	width: 75px;
+	background: #0A5C93;
+	text-align: center;
+	line-height: 50px;
+	color: white; 
+}
+
+.jumbotron {
+	height: 
+	background: rgb(39,48,63);
+
+}
+
+.maintext {
+	text-align: center;
+	margin-top: 165px;
+	font-size: 36px;
+	color: white;
+	font-weight: normal;
+	padding: 0px 30px;
+}
+
+h4 {
+	text-align: center;
+	padding: 0 100px;
+	font-size: 15px;
+	color: white;
+	font-weight: normal;
+}
+
+.jumbotronbutton {
+	text-align: center;
+	height: 50px;
+	color: white;
+	font-size: 25px;
+	background: rgb(21,119,179);
+	margin: 35px auto 0;
+	border: 10px solid rgb(25,30,40);
+	line-height: 50px;
+	width: 210px;
+}
+
+.arrow {
+	margin: 215px auto 135px auto;
+	height: 20px;
+	background: white;
+	width: 50px;
+}
+
+.highlights {
+	margin: 30px 77px 0px 77px;
+	height: 70px;
+	background: white;
+}
+
+.description {
+	height: 190px;
+	background: rgb(226,236,243);
+	margin: 75px 0 0 0;
+}
+
+.features {
+	border-top: 33px solid #C4C6CC;
+	border-left: 10px solid #C4C6CC;
+	border-right: 10px solid #C4C6CC;
+	background: white;
+	height: 215px;
+}
+
+.prefooter {
+	height: 170px;
+	background: #E2ECF3;
+	border-top: 33px solid #C4C6CC;
+}
+
+.prefooterbutton {
+	font-weight: normal;
+	text-align: center;
+	height: 50px;
+	color: white;
+	font-size: 22px;
+	background: rgb(21,119,179);
+	margin: 100px auto 0;
+	line-height: 50px;
+	width: 210px;
+}
+
+footer {
+	height: 90px;
+	background: black;
+	padding-bottom: 40px;
+	text-align: center;
+}
+
+.footerlinks {
+	margin: 0 auto;
+	padding: 10px 60px;
+}
+
+.footerlink {
+	color: white;
+	text-decoration: none;
+	font-size: 10px;
+	text-align: center;
+	display: inline-block;
+	padding: 4px 7px;
+	margin: 0 auto 7px auto;
+}
+
+.footerbutton {
+	height: 25px;
+	background: white;
+	margin: 10px auto 0px auto;
+	width: 135px;
+}
